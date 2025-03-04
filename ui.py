379cd7ccf4d3563daeb5c8beb61f5aeb763da1f2
Mario Lasić <mario.lasic@outlook.com>
2025-03-04 16:16:37 +0100

    Adds on button clicked method

--- ui.py
+++ ui.py
@@ -2,6 +2,7 @@ from PySide6.QtWidgets import (QApplication, QWidget,
                                QVBoxLayout, QHBoxLayout, 
                                QPushButton, QLineEdit, QLabel)
 import sys
+from calculate import Calculate
 
 class Calculator(QWidget):
     def __init__(self):
@@ -17,33 +18,57 @@ class Calculator(QWidget):
         layout = QVBoxLayout()
         layout.addWidget(self.result_field)
 
-        signs = [['AC', 'B', '%', '/'],'X','-','+',['S','0',',', '=']]
+        self.signs = [['AC', '^', '%', '/'],'X','-','+',['S','0','.', '=']]
 
         buttons_layout = QVBoxLayout()
-        counter = 1
+        counter = 7
         for row in range(0,5):
             row_layout = QHBoxLayout()
             for columns in range(0,4):
                 if row == 0:
-                    button = QPushButton(signs[row][columns])
+                    button = QPushButton(self.signs[row][columns])
+                    button.clicked.connect(self.button_clicked)
                     row_layout.addWidget(button)
                 elif row == 4:
-                    button = QPushButton(signs[row][columns])
+                    button = QPushButton(self.signs[row][columns])
+                    button.clicked.connect(self.button_clicked)
                     if columns == 3:
                         button.setObjectName('equals')
                     row_layout.addWidget(button)
                 elif columns == 3:
-                    button = QPushButton(signs[row])
+                    button = QPushButton(self.signs[row])
+                    button.clicked.connect(self.button_clicked)
                     row_layout.addWidget(button)
                 else:
                     button = QPushButton(str(counter))
-                    counter += 1
+                    button.clicked.connect(self.button_clicked)
                     row_layout.addWidget(button)
+                    counter += 1
+            if not row == 0 and not row == 4:
+                counter -= 6
 
             buttons_layout.addLayout(row_layout)
 
         layout.addLayout(buttons_layout)
         self.setLayout(layout)
+
+    def button_clicked(self):
+        button = self.sender()
+        value = button.text()
+        if value == "0" or value == '.':
+            self.result_field.setText(self.result_field.text() + value)
+        elif value not in self.signs[0] and value not in self.signs and value not in self.signs[4]:
+            self.result_field.setText(self.result_field.text() + value)
+        elif value == 'AC':
+            self.restart()
+        elif value != 'S' and value != '=':
+            self.result_field.setText(self.result_field.text() + value)
+        elif value == '=':
+            calculate = Calculate(self.result_field.text())
+            result = calculate.calculate()
+
+    def restart(self):
+        self.result_field.setText('')
 
 
 if __name__ == "__main__":
