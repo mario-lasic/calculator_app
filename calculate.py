bd7912f2112af8fd1a33e7bf1d47eb66b11bb099
Mario Lasić <mario.lasic@outlook.com>
2025-03-05 16:06:40 +0100

    Adds scientific notation handling

--- calculate.py
+++ calculate.py
@@ -1,3 +1,5 @@
+import math
+
 class Calculate():
     def __init__(self, statement):
         self.statement = statement
@@ -8,6 +10,8 @@ class Calculate():
             self.statement = self.statement.replace('X', '*')
             self.statement = self.statement.replace('%', '/100')
             result = eval(self.statement)
+            if isinstance(result, (int, float)) and (result > 1e10 or result < -1e10):
+                return "{:.2e}".format(result)
             return result
         except Exception as e:
             return str(e)
