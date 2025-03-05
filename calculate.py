a498f184d26a0269e096312b2b437a0455e10259
Mario Lasić <mario.lasic@outlook.com>
2025-03-05 16:32:54 +0100

    Final commit

--- calculate.py
+++ calculate.py
@@ -12,6 +12,7 @@ class Calculate():
             result = eval(self.statement)
             if isinstance(result, (int, float)) and (result > 1e10 or result < -1e10):
                 return "{:.2e}".format(result)
+            result = round(result, 3)
             return result
         except Exception as e:
             return str(e)
